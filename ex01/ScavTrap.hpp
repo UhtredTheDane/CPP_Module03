399aef0b83e22b27de47ab1f519e4c5b78112341
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2023-06-25 20:13:12 +0200

    debut ex02

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -1,4 +1,17 @@
 #ifndef SCAVTRAP_HPP
 # define SCAVTRAP_HPP
 
+class ScavTrap: public ClapTrap
+{
+    public:
+        ScavTrap(std::string name);
+
+        ~ScavTrap(void);
+        void guardGate();
+
+        
+
+    private:
+
+};
 #endif
