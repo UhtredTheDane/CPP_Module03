c0039f17f2d11104206bc218ac2b8246e206cfcb
agengemb <32024540+UhtredTheDane@users.noreply.github.com>
2023-06-28 17:43:59 +0200

    fin ex01 appart ?

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -3,9 +3,9 @@
 ScavTrap::ScavTrap(void) 
 {
 	name = "ScavTrap";
-    	hit_points = 100;
-    	energy_points = 50; 
-    	attack_damage = 20;
+    hit_points = 100;
+    energy_points = 50; 
+    attack_damage = 20;
 
     std::cout << "I am the ScavTrap !"<< std::endl;
 }
@@ -40,31 +40,6 @@ void ScavTrap::attack(const std::string& target)
     {
         std::cout << "ScavTrap " << name << " attacks " << target << ", causing " 
             << attack_damage << " points of damage." << std::endl;
-        --energy_points;
-    }
-}
-
-void ScavTrap::takeDamage(unsigned int amount)
-{
-    if (hit_points <= 0)
-        std::cout << "ScavTrap " << name << " is already dead." << std::endl;
-    else
-    {
-        std::cout << "ScavTrap " << name << " takes " << amount << " points of damage." << std::endl;
-        hit_points -= amount;
-    }
-}
-
-void ScavTrap::beRepaired(unsigned int amount)
-{
-    if (hit_points <= 0)
-        std::cout << "ScavTrap " << name << " is already dead, he can't be repaired." << std::endl;
-    else if (energy_points <= 0)
-        std::cout << "ScavTrap " << name << " has not enough energy points to repair." << std::endl;
-    else
-    {
-        std::cout << "ScavTrap " << name << " fix himself of " << amount << " hit points." << std::endl;
-        hit_points += amount;
         --energy_points;
     }
 }
--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,19 +6,15 @@
 class ScavTrap: public ClapTrap
 {
     public:
-    
         ScavTrap(std::string name);
-	ScavTrap(ScavTrap const& toCopy);
-	~ScavTrap(void);
+	    ScavTrap(ScavTrap const& toCopy);
+	    ~ScavTrap(void);
         
         void attack(const std::string& target);
-        void takeDamage(unsigned int amount);
-        void beRepaired(unsigned int amount);
          
         void guardGate(void);
 
     private:
-
         ScavTrap(void);
 };
 #endif
